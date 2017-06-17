dcf6b8cb2c695714b5ef0ee13916d7fbfd90e994
Paul Ray <paul.ray@nrl.navy.mil>
2017-06-16 23:32:52 -0400

    Added event filtering

--- scripts/master_plotter.py
+++ scripts/master_plotter.py
@@ -16,23 +16,33 @@ from eng_plots import eng_plots
 parser = argparse.ArgumentParser(description = "Plot the NICER data nicely.")
 parser.add_argument("infiles", help="Input files", nargs='+')
 parser.add_argument("-s", "--save", help = "Save plots to file", action = "store_true")
-parser.add_argument("-c", "--sci", help = "Makes some nice science plots", action = "store_true")
-parser.add_argument("-e", "--eng", help = "Makes some nice engineering plots", action = "store_true")
-parser.add_argument("-p", "--pi", help = "Use approximate calibration to compute PI", action = "store_true")
+parser.add_argument("--sci", help = "Makes some nice science plots", action = "store_true")
+parser.add_argument("--eng", help = "Makes some nice engineering plots", action = "store_true")
+parser.add_argument("--emin", help="Minimum energy (keV) to keep", default=0.2, type=float)
+parser.add_argument("--emax", help="Minimum energy (keV) to keep", default=12.0, type=float)
 args = parser.parse_args()
 
+# Load files and build events table
 log.info('Reading files')
 tlist = []
-hdr = pyfits.getheader(args.infiles[0])
 for fn in args.infiles:
+    log.info('Reading file {0}'.format(fn))
     tlist.append(Table.read(fn,hdu=1))
 
+log.info('Concatenating files')
 etable = vstack(tlist,metadata_conflicts='silent')
+del tlist
 # Change TIME column name to MET to reflect what it really is
 etable.columns['TIME'].name = 'MET'
+# Add Time column with astropy Time for ease of use
+log.info('Adding time column')
 etime = etable.columns['MET'] + MET0
-# Add Time column with astropy Time for computations
 etable['T'] = etime
+
+# If there are no PI columns, add them with approximate calibration
+if not ('PI' in etable.colnames):
+    log.info('Adding PI')
+    pi = PI(etable)
 
 # Note: To access event flags, use etable['EVENT_FLAGS'][:,B], where B is
 # the bit number for the flag (e.g. FLAG_UNDERSHOOT)
@@ -43,6 +53,19 @@ exposure = etable.meta['EXPOSURE'] * u.s
 log.info('Computing reset rates')
 nresets = reset_rate(etable, IDS)
 reset_rates = nresets/exposure
+
+log.info('Filtering...')
+# Apply filters for good events
+b1 = etable['EVENT_FLAGS'][:,FLAG_SWTRIG] == False
+b2 = etable['EVENT_FLAGS'][:,FLAG_UNDERSHOOT] == False
+b3 = etable['EVENT_FLAGS'][:,FLAG_OVERSHOOT] == False
+b4 = np.logical_and(etable['PI'] > args.emin*PI_TO_KEV, etable['PI']< args.emax*PI_TO_KEV)
+idx = np.where(b1 & b2 & b3 )[0]
+del b1, b2, b3, b4
+filttable = etable[idx]
+
+log.info("Filtering cut {0} events to {1} ({2:.2f}%)".format(len(etable),
+    len(filttable), 100*len(filttable)/len(etable)))
 
 #Making the plots
 if args.eng:
