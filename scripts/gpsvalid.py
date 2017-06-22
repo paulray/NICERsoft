e80281562bca24323e3b8459c9ff61dfcea2ad15
Paul Ray <paul.ray@nrl.navy.mil>
2017-06-22 09:32:03 -0400

    A few tweaks to gpsvalid

--- scripts/gpsvalid.py
+++ scripts/gpsvalid.py
@@ -26,15 +26,19 @@ for fn in filenames:
     tlist.append(Table.read(fn,hdu=1))
     
     etable = vstack(tlist,metadata_conflicts='silent')
-    print(np.where(etable['GPS_SPS_TIME_VALID'] == 0))
+    #print(np.where(etable['GPS_SPS_TIME_VALID'] == 0))
 
     TIMEzerocount = len(np.where(etable['GPS_SPS_TIME_VALID'] == 0)[0])
     TIMEonecount = len(np.where(etable['GPS_SPS_TIME_VALID'] == 1)[0])
+    TIMEdiffcount = len(np.where(etable['GPS_SPS_TIME_VALID'] > 1)[0])
     zerocount = len(np.where(etable['GPS_SPS_VALID'] == 0)[0])
     onecount = len(np.where(etable['GPS_SPS_VALID'] == 1)[0])
 
-    sol = "{0}: SPS_VALID (0 = {1}, 1= {2}), SPS_TIME_VALID (0 = {3}, 1 = {4})".format(fn[48:71],  
-'{0:03}'.format(zerocount), '{0:03}'.format(onecount), '{0:03}'.format(TIMEzerocount), '{0:03}'.format(TIMEonecount))
+    idx = np.where(etable['GPS_SPS_GDOP'] != 0.0)[0]
+    gdop = etable['GPS_SPS_GDOP'][idx].mean()
+
+    sol = "{0}: SPS_VALID (0 = {1:5d}, 1= {2:5d}), SPS_TIME_VALID (0 = {3:5d}, 1 = {4:5d}, >1 = {5:5d}) <GDOP> = {6:.2f}".format(fn[48:71],  
+zerocount, onecount, TIMEzerocount, TIMEonecount, TIMEdiffcount, gdop)
     
     print(sol)
     
