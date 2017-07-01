69707247dab9b8b99c84cdb551d879097ce5eb67
Paul Ray <paul.ray@nrl.navy.mil>
2017-07-01 07:31:15 -0400

    Improvements to issorb

--- nicer/values.py
+++ nicer/values.py
@@ -2,18 +2,21 @@ import numpy as np
 from astropy.time import Time
 
 # Base values for NICER MET and GPS time
+# Both are counted as seconds since these epochs
 MET0 = Time("2014-01-01T00:00:00.0",scale='utc')
 GPS0 = Time("1980-01-06T00:00:00",scale='utc')
 
 # Conversion from PI to keV (PI is in units of 10 eV)
 PI_TO_KEV = 10.0/1000.0
 
+# Bit assignments in the EVENT_FLAGS housekeeping
 FLAG_SLOW = 3
 FLAG_FAST = 4
 FLAG_SWTRIG = 5
 FLAG_OVERSHOOT = 6
 FLAG_UNDERSHOOT = 7
 
+# Array of DET_IDs that are used
 IDS = np.array([0, 1, 2, 3, 4, 5,6, 7,
                 10, 11, 12, 13, 14, 15, 16, 17,
                 20, 21, 22, 23, 24, 25, 26, 27,
--- scripts/issorb.py
+++ scripts/issorb.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from astropy.coordinates import SkyCoord
 from astropy.coordinates.name_resolve import get_icrs_coordinates
 from pyorbital import tlefile
+import argparse
 
 tle160lines = ['1 25544U 98067A   17160.91338884 +.00001442 +00000-0 +29152-4 0  9993',
         '2 25544 051.6425 074.5823 0004493 253.3640 193.9362 15.54003243060621']
@@ -16,14 +17,17 @@ tle167lines = ['1 25544U 98067A   17167.53403196 +.00002711 +00000-0 +48329-4 0 
 
 platform = 'ISS (ZARYA)'
 
-tle160 = tlefile.read(platform,line1=tle160lines[0],line2=tle160lines[1])
-tle167 = tlefile.read(platform,line1=tle167lines[0],line2=tle167lines[1])
+parser = argparse.ArgumentParser(description="Compute ISS orbit geometry vs time")
+parser.add_argument("sourcename",help="Source name to look up coordinates")
+args = parser.parse_args()
 
-# Most recent TLE
-tle = tle167
+# Set up two TLEs so we can compute the precession rate from the
+# change of RA of ascending node with time
+tle1 = tlefile.read(platform,line1=tle160lines[0],line2=tle160lines[1])
+tle2 = tlefile.read(platform,line1=tle167lines[0],line2=tle167lines[1])
 
 print(platform)
-ISSInclination = tle.inclination*u.deg
+ISSInclination = tle2.inclination*u.deg
 print("Inclination = {0:.3f}".format(ISSInclination))
 
 StarboardPoleDec0 = -1*(90.0*u.deg-ISSInclination)
@@ -33,18 +37,18 @@ print("Starboard Orbit Pole Declination {0:.2f}".format(StarboardPoleDec0))
 print("Port Orbit Pole Declination {0:.2f}".format(PortPoleDec0))
 
 # Compute ISS precession rate in degrees per day
-ISSPrecessionRate = (tle167.right_ascension-tle160.right_ascension)/(tle167.epoch_day-tle160.epoch_day)*u.deg/u.d
+ISSPrecessionRate = (tle2.right_ascension-tle1.right_ascension)/(tle2.epoch_day-tle1.epoch_day)*u.deg/u.d
 print("ISS Precession Rate {0:.3f} ({1:.3f} period)".format(ISSPrecessionRate,
     np.abs(360.0*u.deg/ISSPrecessionRate)))
 
-t0 = Time("{0:4d}-01-01T00:00:00".format(int(tle167.epoch_year)+2000),
-    format='isot',scale='utc') + tle167.epoch_day*u.d
-print("t0 = ",t0.isot)
-StarboardPoleRA0 = np.fmod(tle167.right_ascension + 90.0,360.0)*u.deg
+ttle2 = Time("{0:4d}-01-01T00:00:00".format(int(tle2.epoch_year)+2000),
+    format='isot',scale='utc') + tle2.epoch_day*u.d
+print("ttle2 = ",ttle2.isot)
+StarboardPoleRA0 = np.fmod(tle2.right_ascension + 90.0,360.0)*u.deg
 PortPoleRA0 = np.fmod(StarboardPoleRA0 + 180.0*u.deg, 360.0*u.deg)
 
-print("Starboard Pole RA @ t0 = {0:.3f}".format(StarboardPoleRA0))
-print("Port Pole RA @ t0 = {0:.3f}".format(PortPoleRA0))
+print("Starboard Pole RA @ ttle2 = {0:.3f}".format(StarboardPoleRA0))
+print("Port Pole RA @ ttle2 = {0:.3f}".format(PortPoleRA0))
 
 def StarboardPoleDec(t):
     return np.ones_like(t)*StarboardPoleDec0
@@ -53,7 +57,7 @@ def PortPoleDec(t):
     return np.ones_like(t)*StarboardPoleDec0
 
 def StarboardPoleRA(t):
-    return np.fmod(StarboardPoleRA0 + (t-t0).to(u.d)*ISSPrecessionRate, 360.0*u.deg)
+    return np.fmod(StarboardPoleRA0 + (t-ttle2).to(u.d)*ISSPrecessionRate, 360.0*u.deg)
 
 def PortPoleRA(t):
     return np.fmod(StarboardPoleRA(t)+180.0*u.deg, 360.0*u.deg)
@@ -65,20 +69,26 @@ def PortPoleCoord(t):
     return SkyCoord(PortPoleRA(t).value,PortPoleDec(t).value,unit=u.deg,frame="icrs")
 
 now = Time.now()
-
+doy_now = np.float(now.yday.split(':')[1])
+print("Current DOY = {0}".format(np.int(doy_now)))
 print("StarboardPoleRA (now) = {0:.3f}".format(StarboardPoleRA(now)))
 print("PortPoleRA (now) = {0:.3f}".format(PortPoleRA(now)))
 
-PosJ0437 = get_icrs_coordinates('PSRJ0437-4715')
-print("Separation from Starboard Pole = {0:.3f}".format(PosJ0437.separation(StarboardPoleCoord(now))))
-print("Separation from Port Pole = {0:.3f}".format(PosJ0437.separation(PortPoleCoord(now))))
+SourcePos = get_icrs_coordinates(args.sourcename)
+print("\nSource: {0} at {1}, {2}".format(args.sourcename,SourcePos.ra, SourcePos.dec))
+print("Separation from Starboard Pole = {0:.3f}".format(SourcePos.separation(StarboardPoleCoord(now))))
+print("Separation from Port Pole = {0:.3f}".format(SourcePos.separation(PortPoleCoord(now))))
 
 # Plot separation from orbit pole for all of 2017
-times = np.arange(360)*u.d + t0
+doy2017 = np.arange(365.0)
+times = doy2017*u.d + Time("2017-01-01T00:00:00",format='isot',scale='utc')
 fig, ax = plt.subplots()
-seps = PosJ0437.separation(StarboardPoleCoord(times))
-ax.plot((times-t0).to(u.d), seps.to(u.deg))
-ax.set_title('PSR J0437-4715')
+seps = SourcePos.separation(StarboardPoleCoord(times))
+ax.plot(doy2017, seps.to(u.deg))
+ax.plot(doy_now,SourcePos.separation(StarboardPoleCoord(now)),'o')
+ax.set_title(args.sourcename)
 ax.set_xlabel('DOY 2017')
+ax.set_ylim((0.0,180.0))
+ax.grid(True)
 ax.set_ylabel('Angle from Starboard Pole (deg)')
 plt.show()
