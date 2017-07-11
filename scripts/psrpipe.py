ebd7bf502c54631005850b8d04c76f26c0ea1ead
Paul Ray <paul.ray@nrl.navy.mil>
2017-07-11 07:42:58 -0400

    Working on psrpipe

--- scripts/psrpipe.py
+++ scripts/psrpipe.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 from __future__ import print_function, division
 import os
 import matplotlib.pyplot as plt
@@ -17,16 +16,31 @@ parser.add_argument("--obsid", help="Use this as OBSID for directory and filenam
     default=None)
 args = parser.parse_args()
 
-for obsdir in indirs:
+os.environ['HEADASNOQUERY'] = ' '
+os.environ['HEADASPROMPT'] = '/dev/null'
+
+# For some reason if the script is called via #!/usr/bin/env python
+# it does not inherit LD_LIBRARY_PATH so ftools don't run.
+#print(os.environ['LD_LIBRARY_PATH'])
+#print(os.environ['HEADAS'])
+#os.environ['LD_LIBRARY_PATH'] = path.join(os.environ['HEADAS'],'lib')
+#print(os.environ['LD_LIBRARY_PATH'])
+
+def runcmd(cmd):
+    # CMD should be a list of strings since it is not processed by a shell
+    log.info('CMD: '+" ".join(cmd))
+    check_call(cmd,env=os.environ)
+
+for obsdir in args.indirs:
 
     # Set up a basename and make a work directory
     if args.obsid is not None:
         basename = args.obsid
     else:
         # Trim trailing / if needed
-        if args.obsid.endswith("/"):
-            args.obsid = args.obsid[:-1]
-        basename = path.basename(args.obsid).strip()
+        if obsdir.endswith("/"):
+            obsdir = obsdir[:-1]
+        basename = path.basename(obsdir)
 
     # Make directory for working files and output
     pipedir = "{0}_pipe".format(basename)
@@ -36,17 +50,44 @@ for obsdir in indirs:
     # Get event filenames (could be just one)
     evfiles = glob(path.join(obsdir,'xti/event_cl/ni*mpu?_cl.evt'))
     evfiles.sort()
+    log.info('Event Files: {0}'.format("\n" + "    \n".join(evfiles)))
 
     # Get orbit file
     orbfile = glob(path.join(obsdir,'auxil/ni*.orb'))[0]
+    log.info('Orbit File: {0}'.format(orbfile))
 
     # Get filter file
     mkfile = glob(path.join(obsdir,'auxil/ni*.mkf'))[0]
+    log.info('MKF File: {0}'.format(mkfile))
 
     #  Get MPU hk files
     hkfiles = glob(path.join(obsdir,'xti/hk/ni*.hk'))
     hkfiles.sort()
+    log.info('MPU HK Files: {0}'.format("\n" + "    \n".join(hkfiles)))
 
     # Create GTI from .mkf file
-    cmd = "maketime {0} good.gti expr="(SAA.eq.0).and.(FOV_FLAG.eq.0).and.(ANG_DIST.lt.0.02)" compact=no time=TIME clobber=yes".format(mkfile)
-    check_call(cmd,shell=True)
+    mkf_expr='(SAA.eq.0).and.(FOV_FLAG.eq.0).and.(ANG_DIST.lt.0.02)'
+    gtiname = path.join(pipedir,'good.gti')
+    cmd = ["maketime", mkfile, gtiname, 'expr={0}'.format(mkf_expr),
+        "compact=no", "time=TIME", "clobber=yes"]
+    runcmd(cmd)
+
+    # Build input file for ftmerge
+    evlistname=path.join(pipedir,'evfiles.txt')
+    fout = file(evlistname,'w')
+    evfilt_expr = '(PI>30).and.(EVENT_FLAGS==bx1x000)'
+    for en in evfiles:
+        print('{0}[{1}]'.format(en,evfilt_expr),file=fout)
+    fout.close()
+    
+    # Run ftmerge
+    mergedname = path.join(pipedir,"merged.evt")
+    cmd = ["ftmerge", "@{0}".format(evlistname), "outfile={0}".format(mergedname)]
+    runcmd(cmd)
+
+    # Now apply the good GTI to remove SAA and slew time ranges
+    filteredname = path.join(pipedir,"merged_filt.evt")
+    cmd = ["fltime", mergedname, gtiname, filteredname, "copyall=yes"]
+    runcmd(cmd)
+
+    # Add phases and plot, if requested
