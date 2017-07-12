c95d2bba445463d54b4f484d3d2c432334d8cf4a
Paul Ray <paul.ray@nrl.navy.mil>
2017-07-12 13:19:12 -0400

    Added fitsutils

---
+++ nicer/fitsutils.py
@@ -0,0 +1,50 @@
+from __future__ import print_function, division
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+from astropy import log
+from os import path
+from glob import glob
+from subprocess import check_call
+import shutil
+import tempfile
+from astropy.table import Table
+
+from nicer.values import *
+
+def runcmd(cmd):
+    # CMD should be a list of strings since it is not processed by a shell
+    log.info('CMD: '+" ".join(cmd))
+    check_call(cmd,env=os.environ)
+
+def filtandmerge(evfiles):
+    'Merges and filters a set of event files, returning an etable'
+
+    # Might want to specify the directory (with dir=) where this should be.
+    # The default is system-dependents, and likely /tmp
+    tmpdir = tempfile.mkdtemp()
+
+    # Build input file for ftmerge
+    evlistname=path.join(tmpdir,'evfiles.txt')
+    fout = file(evlistname,'w')
+    evfilt_expr = '(PI>30).and.(EVENT_FLAGS==bx1x000)'
+    for en in evfiles:
+        print('{0}[{1}]'.format(en,evfilt_expr),file=fout)
+    fout.close()
+
+    # Run ftmerge
+    mergedname = path.join(tmpdir,"merged.evt")
+    cmd = ["ftmerge", "@{0}".format(evlistname), "outfile={0}".format(mergedname),
+        "clobber=yes"]
+    runcmd(cmd)
+
+    # Read merged event FITS file into a Table
+    etable = Table.read(mergedname,hdu=1)
+
+    # Clean up
+    os.remove(evlistname)
+    os.remove(mergedname)
+    os.rmdir(tmpdir)
+
+    return etable
