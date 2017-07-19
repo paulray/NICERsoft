5d4b0d68aa27086692cf6289efb2a1c05f698294
Faith Huynh <owly@mit.edu>
2017-07-19 13:29:03 -0400

    fixed 2 typos

--- scripts/InteractiveLC.py
+++ scripts/InteractiveLC.py
@@ -15,9 +15,9 @@ class InteractiveLC(object):
         self.binsize = 1.0
 
     def welcome(self):
-        print 'Some code is running, and then you can click on teh plots!'
+        print 'Some code is running, and then you can click on the plots!'
         print 'Click on the plot to choose an initial point'
-        print 'Drag WITHOUT RELEASING to chose a second point'
+        print 'Drag WITHOUT RELEASING to choose a second point'
 
     def __init__(self, etable, lclog, gtitable,  fig, name, binsize):
         self.init()
