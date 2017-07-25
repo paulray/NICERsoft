fddee41d15b603804fa29eaeba60ed5fd3b09a24
Faith Huynh <owly@mit.edu>
2017-07-25 10:23:16 -0400

    lat lon come from mktable

--- nicer/cartographer.py
+++ nicer/cartographer.py
@@ -13,7 +13,7 @@ from nicer.values import *
 from nicer.mcc import MCC
 from nicer.sps import SPS
 
-def cartography(hkmet, overshootrate, args, undershootrate, etable):
+def cartography(hkmet, overshootrate, args, undershootrate, etable, mktable):
     from mpl_toolkits.basemap import Basemap
 #Getting the data
     log.info('Getting SAA data')
@@ -24,9 +24,43 @@ def cartography(hkmet, overshootrate, args, undershootrate, etable):
     #eph = MCC(path.join(datadir,'MCC1_On_Console_20171631440_V01.txt'))
    
     # Use GPS SPS data for ISS location
-    eph = SPS(args.sps)
-    log.info('got SPS ephemeris')
-    lat, lon = eph.latlon(hkmet)
+    
+    lat = mktable['SAT_LAT']
+    lon = mktable['SAT_LON']
+    
+    #Making the overshootrate the same length as lat/lon
+    evenbins = np.histogram(overshootrate, bins = len(lat))[0]
+    sum = []
+    mysum = 0
+
+    for num in evenbins:
+        count = 0
+        for i in xrange(count,count+num):
+            mysum = mysum + overshootrate[i]
+        sum = np.append(sum, (mysum/num))
+        mysum = 0
+        count = count + num
+    
+    overshootrate = sum
+
+    #Making the undershootrate the same length as the lat/lon
+    evenbins = np.histogram(undershootrate, bins = len(lat))[0]
+    sum = []
+    mysum = 0
+        
+    for num in evenbins:
+        count = 0
+        for i in xrange(count,count+num):
+            mysum = mysum + undershootrate[i]
+        sum = np.append(sum, (mysum/num))
+        mysum = 0
+        count = count + num
+
+    undershootrate = sum
+
+    #eph = SPS(args.sps)
+    #log.info('got SPS ephemeris')
+    #lat, lon = eph.latlon(hkmet)
 
 #Creating the plots and figure
     log.info('plotting map')
@@ -39,6 +73,7 @@ def cartography(hkmet, overshootrate, args, undershootrate, etable):
     map = Basemap(projection='cyl', resolution = 'l', lat_0=0, lon_0=0)
     map.drawcoastlines()
     sc = map.scatter(lon, lat,c=overshootrate,norm=LogNorm(vmin=10.0,vmax=1000.0),cmap='jet')
+    #sc = map.scatter(lon,lat)
     map.plot(saa_lon,saa_lat,'r',lw=2)
     map.plot(nph_lon,nph_lat,color='orange',marker='o',markersize=10.0,linestyle='-')
     map.plot(sph_lon,sph_lat,'orange',marker='o',markersize=10.0,linestyle='-')
@@ -56,7 +91,7 @@ def cartography(hkmet, overshootrate, args, undershootrate, etable):
     map.plot(sph_lon,sph_lat,'orange',marker='o',markersize=10.0,linestyle='-')
     cbar = map.colorbar(sc, location='bottom',pad='5%')
     plot.title('Undershoot Rate')
-    #cbar.set_label('Undershoot Rate')
+    cbar.set_label('Undershoot Rate')
     fig.suptitle('ObsID {0}: {1} on {2}'.format(etable.meta['OBS_ID'],
             etable.meta['OBJECT'],etable.meta['DATE-OBS'].replace('T',' at ')),
             fontsize=18)
