48025a3cd9043e6d91319bf2be4e06dea7e2c76d
Paul Ray <paul.ray@nrl.navy.mil>
2017-07-28 11:15:43 -0400

    Fixed mapping

--- nicer/cartographer.py
+++ nicer/cartographer.py
@@ -11,7 +11,8 @@ import matplotlib.gridspec as gridspec
 
 from nicer.values import *
 from nicer.mcc import MCC
-from nicer.sps import SPS
+#from nicer.sps import SPS
+from nicer.latloninterp import LatLonInterp
 
 def cartography(hkmet, overshootrate, args, undershootrate, etable, mktable):
     from mpl_toolkits.basemap import Basemap
@@ -20,61 +21,34 @@ def cartography(hkmet, overshootrate, args, undershootrate, etable, mktable):
     saa_lon, saa_lat = np.loadtxt(path.join(datadir,'saa_lonlat.txt'),unpack=True)
     nph_lon, nph_lat = np.loadtxt(path.join(datadir,'nph_lonlat.txt'),unpack=True)
     sph_lon, sph_lat = np.loadtxt(path.join(datadir,'sph_lonlat.txt'),unpack=True)
-    
+
 
     #eph = MCC(path.join(datadir,'MCC1_On_Console_20171631440_V01.txt'))
-   
+
     # Use GPS SPS data for ISS location
-    
-    lat = mktable['SAT_LAT']
-    lon = mktable['SAT_LON']
-    
-    #Making the overshootrate the same length as lat/lon
-    evenbins = np.histogram(overshootrate, bins = len(lat))[0]
-    sum = []
-    mysum = 0
 
-    for num in evenbins:
-        count = 0
-        for i in xrange(count,count+num):
-            mysum = mysum + overshootrate[i]
-        sum = np.append(sum, (mysum/num))
-        mysum = 0
-        count = count + num
-    
-    overshootrate = sum
-
-    #Making the undershootrate the same length as the lat/lon
-    evenbins = np.histogram(undershootrate, bins = len(lat))[0]
-    sum = []
-    mysum = 0
-        
-    for num in evenbins:
-        count = 0
-        for i in xrange(count,count+num):
-            mysum = mysum + undershootrate[i]
-        sum = np.append(sum, (mysum/num))
-        mysum = 0
-        count = count + num
-
-    undershootrate = sum
+    llinterp = LatLonInterp(mktable['TIME'], mktable['SAT_LAT'], mktable['SAT_LON'])
 
     #eph = SPS(args.sps)
     #log.info('got SPS ephemeris')
     #lat, lon = eph.latlon(hkmet)
 
-#Creating the plots and figure
+    # Interpolate lon, lat from MKF housekeeping data
+    lat, lon = llinterp.latlon(hkmet)
+
+    #Creating the plots and figure
     log.info('plotting map')
     fig = plot.figure(figsize = (9,12), facecolor = 'white')
     mapper = gridspec.GridSpec(19,4)
-    
+
     #fig, ax = plot.subplots(figsize=(16,9))
     overshoot = plot.subplot(mapper[1:10,0:4])
 
     map = Basemap(projection='cyl', resolution = 'l', lat_0=0, lon_0=0)
     map.drawcoastlines()
     sc = map.scatter(lon, lat,c=overshootrate,norm=LogNorm(vmin=10.0,vmax=1000.0),cmap='jet')
-    #sc = map.scatter(lon,lat)
+    #sctest1 = map.scatter(lon, lat,c=np.ones(len(lon))*1000.0,norm=LogNorm(vmin=10.0,vmax=1000.0),cmap='jet')
+    #sctest2 = map.scatter(mktable['SAT_LON'], mktable['SAT_LAT'],c=np.ones(len(mktable['SAT_LON']))*100.0,norm=LogNorm(vmin=10.0,vmax=1000.0),cmap='jet',alpha=0.5)
     map.plot(saa_lon,saa_lat,'r',lw=2)
     map.plot(nph_lon,nph_lat,color='orange',marker='o',markersize=10.0,linestyle='-')
     map.plot(sph_lon,sph_lat,'orange',marker='o',markersize=10.0,linestyle='-')
