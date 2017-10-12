f802dd76ca81838055df270bac2a98e900c7a484
Paul Ray <paul.ray@nrl.navy.mil>
2017-10-12 10:40:37 -0400

    Fixed psrpipe.py description in the code

--- scripts/psrpipe.py
+++ scripts/psrpipe.py
@@ -19,15 +19,15 @@ Output will be written in current working directory in directories that end in '
 
 Several diagnostic plots are produced, and the following data processing steps are run:
 * Select good times according to the following:
-  * (ANG_DIST.lt.0.01).and.(ELV>30.0)
+  * (ANG_DIST.lt.0.015).and.(ELV>30.0)
   * (MODE.eq.1).and.(SUBMODE_AZ.eq.2).and.(SUBMODE_EL.eq.2)
   * SAT_LAT, SAT_LONG not in SAA or polar horn regions specified by region files
-  * If --ultraclean is specified then also filter on SUNSHINE.eq.0
+  * If --dark is specified then also filter on SUNSHINE.eq.0
 Optionally, you can filter on overshoot rate or rate of bad ratio events
 
 * Select events according to:
   * EVENT_FLAGS=bx1x000 (SLOW-only or SLOW+FAST events)
-  * PI in the specified range (default is 0.4-8.0 keV)
+  * PI in the specified range (default is 0.25-12.0 keV)
   * Remove events from any DET_ID specified by the --mask parameter
 
  The final output is 'cleanfilt.evt' and extracted PHA and lightcurve FITS
