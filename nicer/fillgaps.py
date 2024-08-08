5b22bc9f1651b15766fced1931093edb85634c3f
Paul Ray <paul.ray@nrl.navy.mil>
2024-08-08 17:15:52 -0400

    Fixed up fillgaps

--- nicer/fillgaps.py
+++ nicer/fillgaps.py
@@ -12,42 +12,17 @@ import matplotlib.pyplot as plt
 from scipy.fft import fft, ifft
 from itertools import groupby
 from operator import itemgetter
+from loguru import logger as log
 
-def fillgaps(datafile, savefile = "NaN", plotresults = True):
+
+def fillgaps(x, y):
     """A method to fill gaps in data and preserve data noise characteristics.
 
     Parameters:
-    datafile: (string) a file path ending in ".csv" to a comma delimited csv file with two columns formatted as 
-              <time,equispaced>, <data>
-
-    savefile: (string) a file path ending in ".csv" to which to save the imputed results
-    
-    plotresults: (Bool) whether or not to make a plot of the input data and filled data
+    x : array of time values, which should be on a grid, but can be missing points (data gaps)
+    y : array of time offset values for each x
 
     """
-
-    y = []
-    x = np.array([])
-    if datafile[-4:] == ".csv":  # if .csv file entered
-        with open(datafile, "r") as f:
-            lines = f.readline()
-            while lines:
-                vals = lines.split(",")
-                x = np.append(x, float(vals[0]))
-                try:
-                    y.append(float(vals[1]))
-                except:
-                    y.append(np.nan)
-                lines = f.readline()
-    elif datafile[-4:] == ".txt":  # if .txt file entered
-        with open(datafile, "r") as f:
-            lines = f.readline()
-            while lines:
-                vals = lines.strip().split()
-                if vals != []:  # if not an empty line
-                    x = np.append(x, float(vals[0]))
-                    y.append(float(vals[1]))
-                lines = f.readline()
 
     # create equispaced array for x-axis
     if len(x) >= 2:
@@ -66,19 +41,13 @@ def fillgaps(datafile, savefile = "NaN", plotresults = True):
     data[xindices] = y
 
     xfilled = np.linspace(x[0], x[-1], num=int(numintervals + 1))
-    if plotresults:
-        fig, ax = plt.subplots(2, figsize=(12, 8))
-        ax[0].set_title("Original Data")
-        ax[0].scatter(x, y, marker = '.')
-        ax[0].grid(True)
-   
 
     # find indeces of data gaps and largest continuous run of data
     gap_indeces = np.where(np.isnan(data))[0]
     # check if the above numpy.ndarray empty - print message and exit
     if gap_indeces.size == 0:
-        print("The data has no gaps. Exiting...")
-        return
+        print("The data has no gaps")
+        return (xfilled, data)
 
     gap_total = 1
     start_gap_inx = gap_indeces[0]
@@ -134,13 +103,13 @@ def fillgaps(datafile, savefile = "NaN", plotresults = True):
                     maxdiff = diff
                     gap_to_impute = i + 1
 
-    # impute gaps to the right of this run, then reverse and do left 
+    # impute gaps to the right of this run, then reverse and do left
     # i. Type 2 - reflect+invert
 
     while gap_total != 0:
         while gap_to_impute <= gap_total and gap_to_impute != 0:
             gap_to_impute, gap_total = fill(
-            data, gaps, gap_total, gap_to_impute, xfilled 
+                data, gaps, gap_total, gap_to_impute, xfilled
             )
             if gap_to_impute is None:
                 return
@@ -158,19 +127,9 @@ def fillgaps(datafile, savefile = "NaN", plotresults = True):
         for i in range(1, len(gaps)):
             diff = gaps[i + 1][0] - gaps[i][1]
             if diff > maxdiff:
-                gap_to_impute = (
-                    i + 1
-                )  # gap to the right of the largest continuous run
-    if savefile != "NaN":
-        with open(savefile, "w",  newline='') as f:
-            csvwriter = csv.writer(f)
-            csvwriter.writerows(zip(xfilled, data))  # write results into csv file in same format as input
-    if plotresults:
-        ax[1].scatter(xfilled, data, marker='.')
-        ax[1].grid(True)
-        ax[1].set_title("Recovered Data")
-        plt.show()  # displays graphs of data before/after imputation
+                gap_to_impute = i + 1  # gap to the right of the largest continuous run
 
+    return (xfilled, data)
 
 
 def filterfunction(data):
@@ -178,53 +137,62 @@ def filterfunction(data):
     Takes a continuous block of data and multiplies it in the frequency domain by exp(-2^3 f/T)
     (The fft shows the positive frequencies followed by flipped negative frequencies, so we flip the exponential filter and append itself to its end)
     """
-    #pad dataset
-    data = np.append(np.flip(data),np.append(data,np.flip(data)))
-    #fft
+    # pad dataset
+    data = np.append(np.flip(data), np.append(data, np.flip(data)))
+    # fft
     fourierdata = fft(data.T).T
-    #multiply by filter
+    # multiply by filter
     length = np.size(data)
-    if (length%2)==0:
-        filterform = np.exp(-np.arange(0,length/2)*(16/length))
-        filterform = np.append(filterform,np.flip(filterform))
+    if (length % 2) == 0:
+        filterform = np.exp(-np.arange(0, length / 2) * (16 / length))
+        filterform = np.append(filterform, np.flip(filterform))
     else:
-        filterform = np.exp(-np.arange(0,(length-1)/2)*(16/length))
-        filterform = np.append(filterform,np.append(filterform[-1],np.flip(filterform)))
-    filteredfourier = np.multiply(fourierdata.reshape(length,1),filterform.reshape(length,1))
-    #ifft
+        filterform = np.exp(-np.arange(0, (length - 1) / 2) * (16 / length))
+        filterform = np.append(
+            filterform, np.append(filterform[-1], np.flip(filterform))
+        )
+    filteredfourier = np.multiply(
+        fourierdata.reshape(length, 1), filterform.reshape(length, 1)
+    )
+    # ifft
     filtereddata = np.real(ifft(filteredfourier.T).T)
-    #unpad
-    filtereddata = filtereddata[int(length/3):int(2*length/3)]
+    # unpad
+    filtereddata = filtereddata[int(length / 3) : int(2 * length / 3)]
     return filtereddata
+
+
 def filter_all(data):
     """
     Takes a set of data with gaps and applies the filter to each continuous block of data within the dataset
     """
-    #filter individual blocks of continuous data
-    datablocks = [list(v) for k,v in groupby(data,np.isfinite) if k]
+    # filter individual blocks of continuous data
+    datablocks = [list(v) for k, v in groupby(data, np.isfinite) if k]
     filtereddatablocks = [filterfunction(v) for v in datablocks]
-    if np.any(np.isnan(data)): #if there are gaps
-        #get information about gaps
+    if np.any(np.isnan(data)):  # if there are gaps
+        # get information about gaps
         gap_indeces = np.where(np.isnan(data))[0]
         gap_locs = []
         gap_lengths = []
-        for k, g in groupby(enumerate(gap_indeces), lambda ix:ix[1]-ix[0]):
+        for k, g in groupby(enumerate(gap_indeces), lambda ix: ix[1] - ix[0]):
             temprun = list(map(itemgetter(1), g))
             gap_lengths.append(np.size(temprun))
             gap_locs.append(temprun[0])
-        #build filtered data with gaps
-        filteredrun = np.array([]);
-        if gap_locs[0]==0: #if the first space is a gap, append and remove
-            filteredrun = np.append(filteredrun, np.full(gap_lengths[0],np.NaN) )
-            gap_locs = np.delete(gap_locs,0)
-            gap_lengths = np.delete(gap_lengths,0)
-        for i in range(np.size(filtereddatablocks)):
-            filteredrun = np.append(filteredrun,filtereddatablocks[i])
-            if i<np.size(gap_locs):
-                filteredrun = np.append(filteredrun,np.full(gap_lengths[i],np.NaN))
-    else: #if no gaps
-       filteredrun = filterfunction(data)
+        # build filtered data with gaps
+        filteredrun = np.array([])
+        if gap_locs[0] == 0:  # if the first space is a gap, append and remove
+            filteredrun = np.append(filteredrun, np.full(gap_lengths[0], np.NaN))
+            gap_locs = np.delete(gap_locs, 0)
+            gap_lengths = np.delete(gap_lengths, 0)
+        log.info(f"len(filtereddatablocks) = {len(filtereddatablocks)}")
+        log.info(f"{[np.size(bb) for bb in filtereddatablocks]}")
+        for i in range(len(filtereddatablocks)):
+            filteredrun = np.append(filteredrun, filtereddatablocks[i])
+            if i < np.size(gap_locs):
+                filteredrun = np.append(filteredrun, np.full(gap_lengths[i], np.NaN))
+    else:  # if no gaps
+        filteredrun = filterfunction(data)
     return filteredrun
+
 
 def check_right(data, gaps, curgap, curgap_size, curgap_num, gap_total):
     """A method to check the right-hand side of the data to obtain the correct number of points to fill the gap.
@@ -307,7 +275,7 @@ def check_both_sides(data, gaps, curgap, curgap_size, curgap_num, gap_total):
                 data[int(curgap[0] - curgap_size / 2) : (curgap[0])]
             )
             pts_to_reflect_right = copy.deepcopy(
-                data[(curgap[1] + 1) : int(curgap[1] + 1 + curgap_size / 2) ]#+ 1]
+                data[(curgap[1] + 1) : int(curgap[1] + 1 + curgap_size / 2)]  # + 1]
             )
             return pts_to_reflect_left, pts_to_reflect_right
         else:  # not enough space on one/both sides
@@ -326,7 +294,7 @@ def check_both_sides(data, gaps, curgap, curgap_size, curgap_num, gap_total):
                 data[int(curgap[0] - curgap_size / 2) : (curgap[0])]
             )
             pts_to_reflect_right = copy.deepcopy(
-                data[(curgap[1] + 1) : int(curgap[1] + 1 + curgap_size / 2) ]#+ 1]
+                data[(curgap[1] + 1) : int(curgap[1] + 1 + curgap_size / 2)]  # + 1]
             )
             return pts_to_reflect_left, pts_to_reflect_right
         elif (curgap_num != gap_total) and (
@@ -336,7 +304,7 @@ def check_both_sides(data, gaps, curgap, curgap_size, curgap_num, gap_total):
                 data[int(curgap[0] - curgap_size / 2) : (curgap[0])]
             )
             pts_to_reflect_right = copy.deepcopy(
-                data[(curgap[1] + 1) : int(curgap[1] + 1 + curgap_size / 2) ]#+ 1]
+                data[(curgap[1] + 1) : int(curgap[1] + 1 + curgap_size / 2)]  # + 1]
             )
             return pts_to_reflect_left, pts_to_reflect_right
         else:  # not enough space on one/both sides
@@ -381,7 +349,7 @@ def check_left(data, gaps, curgap, curgap_size, curgap_num, gap_total):
         return pts_to_reflect
 
 
-def fill(data, gaps, gap_total, gap_num, xcoords, reverse = False):
+def fill(data, gaps, gap_total, gap_num, xcoords, reverse=False):
     """A method to fill the gap_num-th gap in the data by reflecting and inverting data on one/both sides of the gap.
 
     Parameters:
@@ -396,7 +364,7 @@ def fill(data, gaps, gap_total, gap_num, xcoords, reverse = False):
     size = (curgap[1] + 1) - curgap[0]
     side = None
     # check if there is enough data previous to this gap
-    
+
     if reverse:
         if curgap[1] > len(data) - size:
             # if not enough previous data (right-hand since flipped), check data following gap
@@ -448,37 +416,42 @@ def fill(data, gaps, gap_total, gap_num, xcoords, reverse = False):
             elif type(pts_to_reflect) == tuple:
                 left_pts_to_reflect = pts_to_reflect[0]
                 right_pts_to_reflect = pts_to_reflect[1]
-        
+
     # impute the gap
     if pts_to_reflect is not None:
         if type(pts_to_reflect) == tuple:
-            left_pts_to_reflect = - np.flip(left_pts_to_reflect)
+            left_pts_to_reflect = -np.flip(left_pts_to_reflect)
             filt_left_pts_to_refect = filterfunction(left_pts_to_reflect)
-            right_pts_to_reflect = - np.flip(right_pts_to_reflect)
+            right_pts_to_reflect = -np.flip(right_pts_to_reflect)
             filt_right_pts_to_reflect = filterfunction(left_pts_to_reflect)
-            right_pts_to_reflect = right_pts_to_reflect + (filt_left_pts_to_refect[-1]-filt_right_pts_to_reflect[0]) #match the two reflected data parts in the middle
+            right_pts_to_reflect = right_pts_to_reflect + (
+                filt_left_pts_to_refect[-1] - filt_right_pts_to_reflect[0]
+            )  # match the two reflected data parts in the middle
             if (size / 2) % 2 == 0.5:
                 mid_val = (left_pts_to_reflect[0] + right_pts_to_reflect[-1]) / 2
-                left_pts_to_reflect = np.append(left_pts_to_reflect,mid_val)
-            pts_to_reflect = np.append(left_pts_to_reflect,right_pts_to_reflect) #combine left and right reflections
-        
+                left_pts_to_reflect = np.append(left_pts_to_reflect, mid_val)
+            pts_to_reflect = np.append(
+                left_pts_to_reflect, right_pts_to_reflect
+            )  # combine left and right reflections
+
         else:
             # invert fill points
             pts_to_reflect = -pts_to_reflect
             # reflect the points across the gap
             pts_to_reflect = np.flip(pts_to_reflect)
-        
+
         # Match endpoints
         filt_pts_to_reflect = filterfunction(pts_to_reflect)
         filt_data = filter_all(data)
-        start_diff = filt_data[curgap[0]-1]-filt_pts_to_reflect[0]
-        end_diff = filt_data[curgap[-1]+1]-filt_pts_to_reflect[-1]
-        line_to_add = np.arange(start_diff,end_diff,(end_diff-start_diff)/(size-.1))
-        
-            
+        start_diff = filt_data[curgap[0] - 1] - filt_pts_to_reflect[0]
+        end_diff = filt_data[curgap[-1] + 1] - filt_pts_to_reflect[-1]
+        line_to_add = np.arange(
+            start_diff, end_diff, (end_diff - start_diff) / (size - 0.1)
+        )
+
         # Insert imputed points
         data[curgap[0] : (curgap[1] + 1)] = pts_to_reflect + line_to_add
-        
+
         # remove gap
         if gap_num == gap_total:
             gaps.pop(gap_num)
@@ -490,5 +463,65 @@ def fill(data, gaps, gap_total, gap_num, xcoords, reverse = False):
     return gap_num, gap_total
 
 
+def fillgaps_main(datafile, savefile="NaN", plotresults=True):
+    """A method to fill gaps in data and preserve data noise characteristics.
+
+    Parameters:
+    datafile: (string) a file path ending in ".csv" to a comma delimited csv file with two columns formatted as
+              <time,equispaced>, <data>
+              or a .txt file with two columns separated by whitespace
+
+              The first column is a time, usually in seconds
+              The second column is a time offset (residual) of one clock relative to another
+
+    savefile: (string) a file path ending in ".csv" to which to save the imputed results
+
+    plotresults: (Bool) whether or not to make a plot of the input data and filled data
+
+    """
+
+    y = []
+    x = np.array([])
+    if datafile[-4:] == ".csv":  # if .csv file entered
+        with open(datafile, "r") as f:
+            lines = f.readline()
+            while lines:
+                vals = lines.split(",")
+                x = np.append(x, float(vals[0]))
+                try:
+                    y.append(float(vals[1]))
+                except:
+                    y.append(np.nan)
+                lines = f.readline()
+    elif datafile[-4:] == ".txt":  # if .txt file entered
+        with open(datafile, "r") as f:
+            lines = f.readline()
+            while lines:
+                vals = lines.strip().split()
+                if vals != []:  # if not an empty line
+                    x = np.append(x, float(vals[0]))
+                    y.append(float(vals[1]))
+                lines = f.readline()
+
+    xfilled, data = fillgaps(x, y)
+
+    if savefile != "NaN":
+        with open(savefile, "w", newline="") as f:
+            csvwriter = csv.writer(f)
+            csvwriter.writerows(
+                zip(xfilled, data)
+            )  # write results into csv file in same format as input
+
+    if plotresults:
+        fig, ax = plt.subplots(2, figsize=(12, 8))
+        ax[0].set_title("Original Data")
+        ax[0].scatter(x, y, marker=".")
+        ax[0].grid(True)
+        ax[1].scatter(xfilled, data, marker=".")
+        ax[1].grid(True)
+        ax[1].set_title("Recovered Data")
+        plt.show()  # displays graphs of data before/after imputation
+
+
 if __name__ == "__main__":
-    fillgaps(sys.argv[1], sys.argv[2])
+    fillgaps_main(sys.argv[1], sys.argv[2])
